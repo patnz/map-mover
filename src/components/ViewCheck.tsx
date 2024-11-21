772de35655a9991f2a2d4af8646ec7a99107adc6
Pat Wilson <pat@wilsons.org.nz>
2024-11-21 00:39:40 +0000

    Update gitignore

--- src/components/ViewCheck.tsx
+++ src/components/ViewCheck.tsx
@@ -1,20 +1,27 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 type Props = {}
 
 export default function ViewCheck({}: Props) {
-  const [viewportSize, setViewportSize] = useState('')
+  const [viewportSize, setViewportSize] = useState(
+    `Width: ${window.innerWidth} | Height:${window.innerHeight}`
+  )
   const width = window.innerWidth
   const height = window.innerHeight
   console.log(`Viewport width: ${width}, height: ${height}`)
   window.addEventListener('resize', () => {
     setViewportSize(
-      `Viewport resized to: ${window.innerWidth}x${window.innerHeight}`
+      `Width: ${window.innerWidth} | Height:${window.innerHeight}`
     )
     console.log(
       `Viewport resized to: ${window.innerWidth}x${window.innerHeight}`
     )
   })
 
-  return <div>ViewCheck</div>
+  return (
+    <>
+      <div>{viewportSize}</div>
+      <img src="/central-otago.png" alt="Central Otago" />
+    </>
+  )
 }
